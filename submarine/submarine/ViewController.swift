d5bf83995ce148e48d6e4706d158605e61ab9e31
zhangyang <apple@appleaka.local>
2016-07-28 20:58:47 +0800

    addWebKit

--- submarine/submarine/ViewController.swift
+++ submarine/submarine/ViewController.swift
@@ -7,23 +7,37 @@
 //
 
 import UIKit
+import WebKit
 
-import Alamofire
+class ViewController: UIViewController,WKNavigationDelegate {
 
-
-class ViewController: UIViewController {
-
+    var webView: WKWebView
+    
+  
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.webView = WKWebView(frame: CGRectMake(0, 0, 320, 568))
+        super.init(coder: aDecoder)
+        self.webView.navigationDelegate = self
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-//        let urlStringGetBooks = "https://api.douban.com/v2/book/search"
+        self.view.addSubview(webView)
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        let height = NSLayoutConstraint(item: webView, attribute: .Height, relatedBy: .Equal, toItem: self.view, attribute: .Height, multiplier: 1, constant:0)
+        let width = NSLayoutConstraint(item: webView, attribute: .Width, relatedBy: .Equal, toItem: self.view, attribute: .Width, multiplier: 1, constant: 0)
+        view.addConstraints([height, width])
+        
+        
+        webView.loadRequest(NSURLRequest(URL:NSURL(string:"https://www.baidu.com")!))
         
         
        
         
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
